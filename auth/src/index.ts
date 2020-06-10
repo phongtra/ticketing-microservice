ee7e4bfbbf9f91023014504c90dd163aa2c98235
Phong Tran <aphtr001@edu.xamk.fi>
2020-06-10 18:49:41 +0700

    add dummy route for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { json } from 'body-parser';
 
 const app = express();
 app.use(json());
+
+app.get('/api/users/currentuser', (_req: Request, res: Response) => {
+  res.send('Hello world');
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
