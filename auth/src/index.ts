8f26dd2e641a725018218d15b33b10ff0521213c
Phong Tran <aphtr001@edu.xamk.fi>
2020-06-11 13:44:27 +0700

    scaffold auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/currentUser';
+import { signInRoute } from './routes/signIn';
+import { signOutRoute } from './routes/signOut';
+import { signUpRoute } from './routes/signUp';
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (_req: Request, res: Response) => {
-  res.send('Hello world');
-});
+app.use('/api/users/currentuser', currentUserRouter);
+app.use('/api/users/signin', signInRoute);
+app.use('/api/users/signout', signOutRoute);
+app.use('/api/users/signup', signUpRoute);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
