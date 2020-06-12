b05f84183bcb526d89ead7bb5bcb05707518bea7
Phong Tran <aphtr001@edu.xamk.fi>
2020-06-12 10:45:06 +0700

    add mongo

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/currentUser';
 import { signInRoute } from './routes/signIn';
 import { signOutRoute } from './routes/signOut';
@@ -21,6 +22,20 @@ app.all('*', async (req, res) => {
 });
 app.use(errorHandler);
 
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv-cluster:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    console.log('Connecting to mongodb');
+  } catch (e) {
+    console.error(e);
+  }
+};
+
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
 });
+start();
