c2ae795454dfcb1d47ce284b898b5f28e28a6d72
Phong Tran <aphtr001@edu.xamk.fi>
2020-06-15 17:39:08 +0700

    added cookie session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 import { currentUserRouter } from './routes/currentUser';
 import { signInRoute } from './routes/signIn';
 import { signOutRoute } from './routes/signOut';
@@ -10,7 +11,14 @@ import { errorHandler } from './middlewares/errorHandler';
 import { NotFoundError } from './errors/NotFoundError';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use('/api/users/currentuser', currentUserRouter);
 app.use('/api/users/signin', signInRoute);
