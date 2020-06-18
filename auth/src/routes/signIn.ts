38ad465d96b6ae4e4e091c038213041f92ea5a61
Phong Tran <aphtr001@edu.xamk.fi>
2020-06-18 21:13:18 +0700

    Add sign in validator

--- auth/src/routes/signIn.ts
+++ auth/src/routes/signIn.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 
-import signInValidator from '../validators/signUpValidator';
+import signInValidator from '../validators/signInValidator';
+import { RequestValidationError } from '../errors/RequestValidationErrors';
 
 const router = express.Router();
 
 router.post('/', signInValidator, (req: Request, res: Response) => {
   const error = validationResult(req);
   if (!error.isEmpty()) {
-    return res.status(400).send(error.array());
+    throw new RequestValidationError(error.array());
   }
   const { email, password } = req.body;
 });
