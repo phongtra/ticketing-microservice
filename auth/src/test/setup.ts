281fbb47ce4257747d8a70777ac98137088a4192
Phong Tran <aphtr001@edu.xamk.fi>
2020-06-19 13:47:49 +0700

    add global sign in to test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../App';
+import request from 'supertest';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin: () => Promise<string[]>;
+    }
+  }
+}
 
 let mongo: MongoMemoryServer;
 
@@ -25,3 +34,16 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'Godlike123!';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+  return cookie;
+};
