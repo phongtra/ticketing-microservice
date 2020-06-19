fc654adf091412bf256ea1944cc95f8bc2f63c39
Phong Tran <aphtr001@edu.xamk.fi>
2020-06-19 12:55:34 +0700

    add test for sign out route

--- auth/src/routes/__test__/signIn.test.ts
+++ auth/src/routes/__test__/signIn.test.ts
@@ -1,14 +1,14 @@
 import request from 'supertest';
 import { app } from '../../App';
 
-beforeEach(async () => {
-  await request(app).post('/api/users/signup').send({
-    email: 'test@test.com',
-    password: 'Godlike123!'
-  });
-});
-
 it('returns a 200 on successful sign in', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'Godlike123!'
+    })
+    .expect(201);
   const res = await request(app)
     .post('/api/users/signin')
     .send({ email: 'test@test.com', password: 'Godlike123!' })
@@ -23,6 +23,13 @@ it('returns a 400 with a non-existing email and password', async () => {
     .expect(400);
 });
 it('fails when incorrect password is provide', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'Godlike123!'
+    })
+    .expect(201);
   await request(app)
     .post('/api/users/signin')
     .send({ email: 'test@test.com', password: 'skadhsakdhak' })
