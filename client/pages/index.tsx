9bf318a95248571082fb5b345417992d6d716cf2
Phong Tran <aphtr001@edu.xamk.fi>
2020-06-22 01:52:52 +0700

    add initial props

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,13 @@
-export default () => {
+import { NextPage } from 'next';
+
+const LandingPage: NextPage<{ color: string }> = ({ color }) => {
+  console.log('I am in the browser', color);
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = () => {
+  console.log('I am on the server');
+  return { color: 'red' };
+};
+
+export default LandingPage;
