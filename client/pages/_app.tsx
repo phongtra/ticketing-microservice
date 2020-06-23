19b247e97bb8614bee1e6141fc62fec135404f8d
Phong Tran <aphtr001@edu.xamk.fi>
2020-06-23 15:01:40 +0700

    add getinitialprops in _app

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,18 @@
 import { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/buildClient';
+import { AxiosResponse } from 'axios';
+import { CurrentUser } from '../types';
+import { AppContextType } from 'next/dist/next-server/lib/utils';
 
-export default ({ Component, pageProps }: AppProps) => {
+const AppComponent = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
+};
+
+AppComponent.getInitialProps = async ({ ctx }: AppContextType) => {
+  const client = buildClient(ctx);
+  const { data }: AxiosResponse<CurrentUser> = await client.get(
+    '/api/users/currentuser'
+  );
+  return data;
 };
