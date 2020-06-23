d75a6bf729b64bbeb67167ebd64f4a386b81ddc9
Phong Tran <aphtr001@edu.xamk.fi>
2020-06-23 15:21:26 +0700

    fetch user in _app

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -5,14 +5,30 @@ import { AxiosResponse } from 'axios';
 import { CurrentUser } from '../types';
 import { AppContextType } from 'next/dist/next-server/lib/utils';
 
-const AppComponent = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({
+  Component,
+  pageProps,
+  currentUser
+}: AppProps & CurrentUser) => {
+  return (
+    <div>
+      <h1>Hello, {currentUser.email}</h1>
+      <Component {...pageProps} />;
+    </div>
+  );
 };
 
-AppComponent.getInitialProps = async ({ ctx }: AppContextType) => {
+AppComponent.getInitialProps = async ({ ctx, Component }: AppContextType) => {
   const client = buildClient(ctx);
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
   const { data }: AxiosResponse<CurrentUser> = await client.get(
     '/api/users/currentuser'
   );
-  return data;
+
+  return { pageProps, data };
 };
+
+export default AppComponent;
