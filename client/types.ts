9b3347a2424c5049edc1f975ca37be0e386aaff9
Phong Tran <aphtr001@edu.xamk.fi>
2020-06-23 15:01:24 +0700

    add signin page

---
+++ client/types.ts
@@ -0,0 +1,16 @@
+import { AxiosResponse } from 'axios';
+
+export interface CurrentUser {
+  currentUser: {
+    id: string;
+    email: string;
+    iat?: number;
+  };
+}
+
+export interface Request {
+  url: string;
+  method: string;
+  body: { [key: string]: any };
+  onSuccess?: (data: AxiosResponse) => void;
+}
