5252b3c009a812883dbc97f24edb780a0f63cba1
Phong Tran <aphtr001@edu.xamk.fi>
2020-06-27 14:35:57 +0700

    scaffold ticket service

---
+++ tickets/src/index.ts
@@ -0,0 +1,17 @@
+import http from 'http';
+import { app } from './App';
+import { DbConnect } from './DbConnect';
+
+const server = http.createServer(app);
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+  DbConnect();
+  server.listen(3000, () => {
+    console.log('Listening on port 3000!');
+  });
+};
+
+start();
---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,26 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'adhaskdhwuihqewuihweuhwi';
+  mongo = new MongoMemoryServer();
+  const mongoURL = await mongo.getUri();
+  await mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
