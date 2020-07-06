9bbafab77e369894b11c5c96f355fc7425d38094
Phong Tran <aphtr001@edu.xamk.fi>
2020-07-06 14:53:24 +0700

    make publish function asynchronous

--- nat-tests/src/events/Publisher.ts
+++ nat-tests/src/events/Publisher.ts
@@ -9,8 +9,11 @@ interface Event {
 export abstract class Publisher<T extends Event> {
   protected abstract subject: T['subject'];
   constructor(private client: Stan) {}
-  publish(data: T['data']) {
-    this.client.publish(this.subject, JSON.stringify(data), () => {
+  async publish(data: T['data']) {
+    this.client.publish(this.subject, JSON.stringify(data), (err) => {
+      if (err) {
+        throw err;
+      }
       console.log(`${this.subject} event published`);
     });
   }
--- nat-tests/src/publisher.ts
+++ nat-tests/src/publisher.ts
@@ -7,7 +7,7 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to nats');
 
   const data = {
@@ -15,5 +15,9 @@ stan.on('connect', () => {
     title: 'connect',
     price: 20
   };
-  new TicketCreatedPublisher(stan).publish(data);
+  try {
+    await new TicketCreatedPublisher(stan).publish(data);
+  } catch (e) {
+    console.log(e);
+  }
 });
