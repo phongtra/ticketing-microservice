5cc3f1abff336da73422c6610a230c95a66f3d5d
Phong Tran <aphtr001@edu.xamk.fi>
2020-08-01 18:15:05 +0300

    fixed listener service

--- orders/src/events/listeners/TicketCreatedListener.ts
+++ orders/src/events/listeners/TicketCreatedListener.ts
@@ -1,17 +1,24 @@
+// import { Listener, TicketCreatedEvent, Subjects } from '@pt-ticket/common';
+// import { Message } from 'node-nats-streaming';
+// import { QUEUE_GROUP_NAME } from '../../constants';
+// import { Ticket } from '../../models/Ticket';
+// export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+//   protected readonly subject = Subjects.TicketCreated;
+
 import { Listener, TicketCreatedEvent, Subjects } from '@pt-ticket/common';
 import { Message } from 'node-nats-streaming';
-import { QUEUE_GROUP_NAME } from '../../constants';
+// import { QUEUE_GROUP_NAME } from '../../constants';
 import { Ticket } from '../../models/Ticket';
+
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   protected readonly subject = Subjects.TicketCreated;
-
-  protected queueGroupName = QUEUE_GROUP_NAME;
-  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
-    console.log('listened');
-
+  protected queueGroupName = 'orderService';
+  protected onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     const { id, title, price } = data;
     const ticket = Ticket.build({ id, title, price });
-    await ticket.save();
-    msg.ack();
+    ticket.save().then(() => {
+      console.log('ticket saved');
+      msg.ack();
+    });
   }
 }
--- orders/src/events/listeners/TicketUpdatedListener.ts
+++ orders/src/events/listeners/TicketUpdatedListener.ts
@@ -5,16 +5,18 @@ import { Ticket } from '../../models/Ticket';
 
 export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   protected readonly subject = Subjects.TicketUpdated;
-  protected queueGroupName = QUEUE_GROUP_NAME;
-  async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
-    console.log('listened');
-    const ticket = await Ticket.findById(data.id);
-    if (!ticket) {
-      throw new Error('Ticket is not found');
-    }
-    const { title, price } = data;
-    ticket.set({ title, price });
-    await ticket.save();
-    msg.ack();
+  protected queueGroupName = 'orderService';
+  protected onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+    Ticket.findById(data.id).then((ticket) => {
+      if (!ticket) {
+        throw new Error('Ticket is not found');
+      }
+      const { title, price } = data;
+      ticket.set({ title, price });
+      ticket.save().then(() => {
+        console.log('ticket updated');
+        msg.ack();
+      });
+    });
   }
 }
